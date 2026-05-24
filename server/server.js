b1157e611a2876dc70b57440fc02fca44cca56a8
kriscsv <krisangelgaufo.fortin24@bicol-u.edu.ph>
2026-05-25 02:36:12 +0800

    updated routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors       = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
+const documentRoutes = require('./routes/documentRoutes');
 
 const app  = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +14,7 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('Server is running!'));
 
 app.use('/api/auth', authRoutes);
+app.use('/api/documents', documentRoutes);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
